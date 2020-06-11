0e65b154fe3c7e0d3358e10834a5005d51714d27
mathieu <mathieumimmsboyce@gmail.com>
2020-06-11 00:17:17 -0400

    Add docstrings to methods.

--- src/cvclip/methods.py
+++ src/cvclip/methods.py
@@ -1,4 +1,10 @@
 def strip_comments(strip_text):
+    """
+    This method will remove the comments from the text provided
+
+    :param strip_text: A string representing the text to remove comments from
+    :return: A string with all comments removed
+    """
     lines = strip_text.split("\n")
 
     stripped = ""
@@ -25,6 +31,15 @@ def strip_comments(strip_text):
 
 
 def replace_titles(company_title, position_title, not_replaced):
+    """
+    This method will replace company title placeholders ($CT) and position title placeholders ($PT)
+    found in the text. It will first strip it of comments and then alter what is left.
+
+    :param company_title: A string representing the title of the company to replace instances of $CT
+    :param position_title: A string representing the title of the position to replace instances of $PT
+    :param not_replaced: A string representing the text that needs its placeholders replaced
+    :return: A string with all placeholders replaced
+    """
     stripped = strip_comments(not_replaced)
     replaced = ""
 
